4110dbeee4d32c684c2f36105b439a4405eaa99e
Jodl Gayatin <jodl.gayatin@gmail.com>
2016-11-22 12:26:46 +0800

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -8,13 +8,13 @@ fileUrl <-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20
 download.file(fileUrl,destfile="./data/data2.zip",method="curl")
 unzip(zipfile = "./data/data2.zip",exdir="./data")
 
-## Reading the data
+## Reading the training data
 
 x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
 y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
 subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 
-# Reading testing tables
+# Reading test data, features and activity labels
 
 x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
@@ -22,40 +22,45 @@ subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
 features <- read.table('./data/UCI HAR Dataset/features.txt')
 activityLabels = read.table('./data/UCI HAR Dataset/activity_labels.txt')
 
-## Assigning column names
+## This part of the code "1) merges the training and the test sets to create one data set."
 
 x_set <- rbind(x_train,x_test)
 y_set <- rbind(y_train,y_test)
 subject_set <- rbind(subject_train,subject_test)
 
-y_set[,1] <- activityLabels[y_set[,1],2]
-
+## This part of the code "2) Extracts only the measurements on the mean and standard deviation for each measurement."
+## Only variables that contain the expression "mean" and "std" were filtered.
 
 colnames(x_set) <- features[,2]
+columnNames <- colnames(x_set)
+
+mean_sd <-(grepl("mean.." , columnNames) | grepl("std.." , columnNames) )
+x_setMeanSd<- x_set[,mean_sd==TRUE]
+
+## This part of the code "3) Uses descriptive activity names to name the activities in the data set
+
+y_set[,1] <- activityLabels[y_set[,1],2]
 colnames(y_set)<- "activityType"
 colnames(subject_set) <- "subjectId"
 
-merge_all<-cbind(x_set,y_set,subject_set)
+merge_all<-cbind(x_setMeanSd,y_set,subject_set)
 
-columnNames <- colnames(merge_all)
+## This part of the code "4) Appropriately labels the data set with descriptive variable names."
+## There was not much changes because the variable names seemed explanatory.
+## I just eliminated the extra "Body" in some variables and converted t to Time and f to Frequency
+## as explained in the Data Set information
 
-mean_sd <-(grepl("activityType" , columnNames) | 
-             grepl("subjectId" , columnNames) | 
-             grepl("mean.." , columnNames) | 
-             grepl("std.." , columnNames) 
-)
+newcolnames<-gsub("BodyBody","Body",colnames(merge_all))
+newcolnames<-gsub("tBody","TimeBody",newcolnames)
+newcolnames<-gsub("tGravity","TimeGravity",newcolnames)
+newcolnames<-gsub("fBody","FreqBody",newcolnames)
 
-subsetMeanSd<- merge_all[,mean_sd==TRUE]
-##subsetNames<- merge(subsetMeanSd, activityLabels,by='activityId',all.x = TRUE)
+colnames(merge_all)<-newcolnames
+
+# This part of the code " 5) creates a second, independent tidy data set with the average of each variable for each activity and each subject."
+
 library(dplyr)
 library(tidyr)
-tidySet <- aggregate(.~subjectId+activityType, subsetMeanSd,mean)
+tidySet <- aggregate(.~subjectId+activityType,merge_all,mean)
 tidySet<-tidySet[order(tidySet$subjectId,tidySet$activityType),]
-
-newcolnames<-gsub("BodyBody","Body",colnames(tidySet))
-newcolnames<-gsub("tBody","TotalBody",newcolnames)
-newcolnames<-gsub("tGravity","TotalGravity",newcolnames)
-## next step is to use gsub to subsitute the title
-## never use a function name as a variable
-colnames(tidySet)<-newcolnames
 write.table(tidySet, "Tidy.txt", row.names = FALSE, quote = FALSE)
